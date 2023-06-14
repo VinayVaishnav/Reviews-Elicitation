2badfb7d7f5b7f6cdab381fe8a02c644f07c644d
VinayVaishnav <115418485+VinayVaishnav@users.noreply.github.com>
2023-06-14 18:28:39 +0530

    Added urls and templates for main app

--- ReviewsElicitation/main/urls.py
+++ ReviewsElicitation/main/urls.py
@@ -6,5 +6,15 @@ from django.conf.urls.static import static
 app_name = 'main'
 
 urlpatterns = [
+    path('',views.login,name='login'),
+    path('signup//',views.signup, name='signup'),
+    path('home/',views.home,name='home'),
+    path('verify/',views.verify,name='verify'),
+    path('logout/',views.logout,name='logout'),
+    path('update/',views.update,name='update'),
+    path('search/',views.search,name='search'),
+    path('user/<str:username>/',views.user,name='user'),
+    path('edit/<int:review_id>',views.edit,name='edit'),
+    path('delete/<int:review_id>',views.delete,name='delete')
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
