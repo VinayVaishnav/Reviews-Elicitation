3859b65636ab50b4bfa10dd3ed8b6425ebb9be6b
yorozuya-2003 <pagaria.2@iitj.ac.in>
2023-07-27 20:10:09 +0530

    Added dropdown in navbar for account settings Also changed color scheme

--- main/polls.py
+++ main/polls.py
@@ -1,54 +1,66 @@
 question_option_dict = {
     1: {
-        'question': 'What is your favorite color?',
+        'question': "How would you describe this person's communication style?",
         'options': [
             {
                 'option_num': 1,
-                'option_text': 'Red'
+                'option_text': 'Straight to the point'
             },
             {
                 'option_num': 2,
-                'option_text': 'Blue'
+                'option_text': 'Listens more, talks less'
             },
             {
                 'option_num': 3,
-                'option_text': 'Green'
+                'option_text': 'Provides constructive feedback'
+            },
+            {
+                'option_num': 4,
+                'option_text': 'Always has a positive or encouraging word'
             },
         ]
     },
     
     2: {
-        'question': 'Question 2',
+        'question': 'How does this person typically spends their lunch break at work?',
         'options': [
             {
                 'option_num': 1,
-                'option_text': 'Option 1'
+                'option_text': 'Chatting with colleagues'
             },
             {
                 'option_num': 2,
-                'option_text': 'Option 2'
+                'option_text': 'Catching up on industry news'
             },
             {
                 'option_num': 3,
-                'option_text': 'Option 3'
+                'option_text': 'Relaxing and unwinding'
+            },
+            {
+                'option_num': 4,
+                'option_text': 'Doing a quick workout'
             },
         ]
     },
 
     3: {
-        'question': 'Question 3',
+        'question': 'How does this person approach challenging situations?',
         'options': [
             {
                 'option_num': 1,
-                'option_text': 'Option 1'
+                'option_text': 'Thinks before acting'
             },
             {
                 'option_num': 2,
-                'option_text': 'Option 2'
+                'option_text': 'Collaborates with others'
             },
             {
                 'option_num': 3,
-                'option_text': 'Option 3'
+                'option_text': 'Takes immediate action'
+            },
+            {
+                'option_num': 4,
+                'option_text': 'Seeks assistance when needed'
             },
         ]
     },
